5202f35da0ee707f0bb877c1d37e0814219b1fd4
AnitaPK <anita.khamitkar@gmail.com>
2025-12-03 11:06:36 +0530

    taskMgmt - III

--- fullstack/TaskManagement/server/controllers/taskController.js
+++ fullstack/TaskManagement/server/controllers/taskController.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize')
 const Task = require('../models/taskModel')
 
 
@@ -17,34 +18,88 @@ async function createTask(req,res){
 }
 async function getAllTasks(req,res){
     try {
-        const tasks = await Task.findAll()
+        const tasks = await Task.findAll({
+            attributes:["id", "title", "status", "startDate","endDate"]
+        })
          res.status(200).send({taks:tasks, success:true})
     } catch (error) {
         res.status(500).send({msg:"Server Error",success:false})
     }
 }
-function getTaskByID(req,res){
+
+async function getTaskByID(req,res){
+    console.log(req.params.ID)
+    console.log(typeof(req.params.ID),"typeOf")
+    const id = Number(req.params.ID)
+    console.log(typeof(id),"typeOf")
+
     try {
-        
+        const task = await Task.findByPk(id)
+        console.log(task,"*******")
+        if(!task){
+            res.status(400).send({msg:"Task not found", success:false})
+        }else{
+        res.status(200).send({success:true, task:task})
+        }
     } catch (error) {
-        res.status(500).send({msg:"Server Error"})
+        res.status(500).send({msg:"Server Error",success:false})
     }
 }
 
-function updateTask(req,res){
+async function queryTaskTitle(req,res) {
+    console.log(req.query)
+    const {tName} = req.query
+    try{   
+        const taskByTitle = await Task.findOne({
+            where:{title:tName},
+            // attributes:{}
+        })
+
+        if(!taskByTitle){
+            res.status(400).send({msg:"Task not found", success:false})
+        }
+        res.status(200).send({success:true, task:taskByTitle})
+
+     } catch (error) {
+        res.status(500).send({msg:"Server Error",success:false})
+    }
+}
+
+
+async function updateTask(req,res){
+    console.log(req.body)
+    console.log(req.params)
+    const ID = req.params.ID
+    const {priority, startDate,endDate} = req.body
     try {
-        
+        const [updatedTask] = await Task.update(
+            {priority, startDate,endDate},
+            {where:{id:ID}}
+        )
+        if(updatedTask === 0){
+            res.status(400).send({msg:"task not found"})
+        }
+        res.status(200).send({success:true, msg:"task updated successfully"})
     } catch (error) {
         res.status(500).send({msg:"Server Error"})
     }
 }
-function deleteTask(req,res){
-            try {
-        
+async function deleteTask(req,res){
+    console.log(req.params)
+    const {ID} = req.params
+    try {
+        const taskDeleted = await Task.destroy({
+            where:{id:ID}
+        })
+        console.log(taskDeleted,'taskDeleted')
+        if(!taskDeleted){
+            res.status(400).send({msg:"Task not found", success:false})
+        }
+        res.status(200).send({msg:"Task deleted successfully", success:true})
     } catch (error) {
         res.status(500).send({msg:"Server Error"})
     }
 }
 module.exports ={
-    createTask, getAllTasks, getTaskByID, updateTask, deleteTask
+    createTask, getAllTasks, getTaskByID, updateTask, deleteTask, queryTaskTitle
 }
--- fullstack/TaskManagement/server/routes/taskRoute.js
+++ fullstack/TaskManagement/server/routes/taskRoute.js
@@ -7,9 +7,15 @@ const taskController = require('../controllers/taskController')
 
 router.post('/createTask', taskController.createTask)
 router.get('/getAllTasks', taskController.getAllTasks)
-// router.get('/getTaskByID/:ID', taskController.getTaskByID)
-// router.put('/updateTask/:ID', taskController.updateTask)
-// router.delete('/deleteTask/:ID', taskController.deleteTask)
+router.get('/getTaskByID/:ID', taskController.getTaskByID)
+router.put('/updateTask/:ID', taskController.updateTask)
+router.delete('/deleteTask/:ID', taskController.deleteTask)
+
+router.get('/queryTaskTitle/', taskController.queryTaskTitle)
+
+//find completed tasks
+//find highest priority tasks
+// find tasks which are completed between given dates 
 
 // router.get('tasksOfUSer',)
 
